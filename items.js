4fcd715fd2f6c63704a6eac7c96e80e0c65a9aca
rajaniraju <rajaniraju2003@gmail.com>
2022-05-09 15:04:16 -0500

    cleanup

--- items.js
+++ items.js
@@ -15,53 +15,53 @@ function getValue() {
 }
 
 function changeImages() {
-//getting the currently selected item.And change images accordingly.
+    //getting the currently selected item.And change images accordingly.
     itemSelected = getValue();
-    let itemDisplay = currentItems.map((item)=>{return(`<div class="mix col-sm-3 page1 page4 margin30">
-    <div class="item-img-wrap ">
-        <img src="${item.imageUrl}" height="250" class="img-responsive" alt="workimg">
-        <div class="item-img-overlay">
+    let itemDisplayMarkup = getMarkup();
+    document.getElementById("container").innerHTML = ` <div id="grid" class="row">${itemDisplayMarkup}</div>`;
 
-        </div>
-    </div> 
-</div>`)});
-    document.getElementById("container").innerHTML = ` <div id="grid" class="row">${itemDisplay}</div>`;
-    
 }
 
 async function getImage() {
     let response = await fetch(url);
     items = await response.json();
     console.log(items);
-    for (i = 0; i < items.length; i++){
+    for (i = 0; i < items.length; i++) {
         let imageSubType = items[i].imageSubType;
         subType.push(imageSubType);
         //inorder to get unique elements in the array.
         let unique = [...new Set(subType)];
-            uniqueSubtypes = unique
-        }
-        console.log(uniqueSubtypes);
-
+        uniqueSubtypes = unique
+    }
+    console.log(uniqueSubtypes);
 
     //getting uniqueSubtype in selection dropdown.
-    let selectionDisplay = uniqueSubtypes.map((subtype)=>{return(` 
+    let selectionDisplay = uniqueSubtypes.map((subtype) => {
+        return (` 
     <option>${subtype}</option>
-    `)})
+    `)
+    })
     document.getElementById("selection").innerHTML = selectionDisplay;
 
     //displaying images accordingly 
-     itemSelected = getValue();
-    let itemDisplay = currentItems.map((item) => {
+    itemSelected = getValue();
+    let itemDisplayMarkup = getMarkup();
+    document.getElementById("container").innerHTML = ` <div id="grid" class="row">${itemDisplayMarkup}</div>`;
+
+}
+
+function getMarkup() {
+    let itemDisplayMarkup = currentItems.map((item) => {
         return (`<div class="mix col-sm-3 page1 page4 margin30">
     <button id="modal">
     <div class="item-img-wrap ">
         <img src="${item.imageUrl}" height="250" class="img-responsive" alt="workimg">
         <div class="item-img-overlay">
-
         </div>
     </div> 
     </button>
-</div>`)});
-    document.getElementById("container").innerHTML = ` <div id="grid" class="row">${itemDisplay}</div>`;
-         
+    </div>`)
+    });
+
+    return itemDisplayMarkup;
 }
