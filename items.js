fd58db2ab2f1efe489d806804b845d926022173b
rajaniraju <rajaniraju2003@gmail.com>
2022-05-10 08:47:22 -0500

    Pagination mark up

--- items.js
+++ items.js
@@ -18,7 +18,9 @@ function changeImages() {
     //getting the currently selected item.And change images accordingly.
     itemSelected = getValue();
     let itemDisplayMarkup = getMarkup();
+    
     document.getElementById("container").innerHTML = ` <div id="grid" class="row">${itemDisplayMarkup}</div>`;
+    
 }
 //the below funcion is called when page is loaded.
 async function getImage() {
@@ -46,7 +48,13 @@ async function getImage() {
     itemSelected = getValue();
     let itemDisplayMarkup = getMarkup();
     document.getElementById("container").innerHTML = ` <div id="grid" class="row">${itemDisplayMarkup}</div>`;
-
+    let paginationDisplayMarkup = getPagination();
+    document.getElementById("bottom-pagination").innerHTML = `${paginationDisplayMarkup}`
+    const list_element = document.getElementById("elementList");
+    const pagination_element = document.getElementsByClassName("pagination");
+    let currentPage = 1;
+    let rows = 1;
+    displayList(currentItems, list_element, rows, currentPage);
 }
 //to display the html contents
 function getMarkup() {
@@ -57,11 +65,62 @@ function getMarkup() {
         <img src="${item.imageUrl}" height="250">
     </div> 
     </button>
-    </div>`)
+    </div>
+    `)
     });
-
+    
     return itemDisplayMarkup;
+    
 }
+function displayList( currentItems,wrapper,rows_per_page,page) {
+
+    wrapper.innerHTML = "";
+    page--;
+    let pageCount = Math.ceil(currentItems.length / rows_per_page);
+    let start = rows_per_page * page;
+    let end = start * rows_per_page;
+    let paginatedItems = currentItems.slice(start, end);
+    console.log(paginatedItems);
+    for (let i = 0; i < paginatedItems.length; i++){
+       
+        let item = paginatedItems[i];
+        console.log(item);
+        let item_element = document.createElement("div");
+    }
+}
+function getPagination() {
+    
+    
+    let paginationMarkup = 
+    ( `
+              <div class="col-sm-6">
+                  <ul class="pagination">
+                      <li>
+                          <a href="#" aria-label="Previous">
+                              <span aria-hidden="true">«</span>
+                          </a>
+                      </li>
+                      <li id="elementList" class="active"><a href="#">1</a></li>
+                      <li><a href="#">2</a></li>
+                      <li><a href="#">3</a></li>
+                      
+                      <li>
+                          <a href="#" aria-label="Next">
+                              <span aria-hidden="true">»</span>
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+              <div class="col-sm-6 text-right">
+                  <em>Displaying 1 to 7 (of 50 posts)</em>
+                 
+              </div>
+              
+         `)
+         return paginationMarkup;
+    }
+    
+
 //to display the model when image is clicked
 function onImageClicked(url,imageType, imageSubType,filename,filepath, imageSize) { 
     console.log(url);
